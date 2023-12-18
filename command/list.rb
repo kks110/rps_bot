7afbb4e94fcc0dcc56fbb03ed414087212c4e8ae
Kelvin Samuel <kelvin@kks110.com>
2023-12-18 18:53:59 +0000

    add a way to see challenges

--- command/list.rb
+++ command/list.rb
@@ -3,11 +3,13 @@
 require_relative 'rps_register'
 require_relative 'create_challenge'
 require_relative 'accept_challenge'
+require_relative 'see_challenges'
 
 module Command
   LIST = [
     RPSRegister.new,
     CreateChallenge.new,
-    AcceptChallenge.new
+    AcceptChallenge.new,
+    SeeChallenges.new
   ].freeze
 end
---
+++ command/see_challenges.rb
@@ -0,0 +1,47 @@
+require_relative 'base'
+
+module Command
+  class SeeChallenges < Command::Base
+    def name
+      :see_challenges
+    end
+
+    def description
+      'See your game challenges!'
+    end
+
+    def execute(request:)
+      event = request.event
+      user = event.user
+      player = Player.find_by({discord_id: user.id})
+
+      people_you_have_challenged = Challenge.where({ challenger: player })
+      people_who_have_challenged_you = Challenge.where({ challenged: player })
+
+      event.respond(
+        content: "#{build_your_sent_challenges(challenges: people_you_have_challenged)} #{build_your_received_challenges(challenges: people_who_have_challenged_you)}",
+        ephemeral: true
+      )
+    end
+
+    def build_your_sent_challenges(challenges:)
+      response = "You have challenged:\n"
+      return response if challenges.nil?
+
+      challenges.each do |challenge|
+        response << "#{challenge.challenged.username}\n"
+      end
+      response
+    end
+
+    def build_your_received_challenges(challenges:)
+      response = "You have challenges from:\n"
+      return response if challenges.nil?
+
+      challenges.each do |challenge|
+        response << "#{challenge.challenger.username}\n"
+      end
+      response
+    end
+  end
+end
